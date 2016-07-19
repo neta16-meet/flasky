428ea85fae659e8cf9b4ab35b4fad2b8b38c23a8
Neta <netarvd@gmail.com>
2016-07-19 14:39:50 +0300

    minor stuff

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,10 @@ def index():
 @app.route('/user/<name>')
 def user(name):
 	return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found():
+	return render_template(404.html, 404)
 
 if __name__ == '__main__':
 	app.run(debug=True)
